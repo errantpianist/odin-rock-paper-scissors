a7cb989f9c6aeb4aba2683a71040f1ce8a8f9113
Yu Zhou Zhang <yuzspanish@gmail.com>
2022-12-26 12:14:20 +0000

    implement win logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,3 +15,68 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+function getWinStatus(playerSelection, computerSelection) {
+    let status;
+    switch (playerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'rock':
+                    status = 'draw';
+                    break;
+                case 'paper':
+                    status = 'lose';
+                    break;
+                case 'scissors':
+                    status = 'win';
+
+                    break;
+            
+                default:
+                    break;
+            }
+            break;
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    status = 'win';
+                    break;
+                case 'paper':
+                    status = 'draw';
+                    break;
+                case 'scissors':
+                    status = 'lose';
+                    break;
+            
+                default:
+                    break;
+            }
+            break;
+        case 'scissors':
+        switch (computerSelection) {
+            case 'rock':
+                status = 'lose';    
+                break;
+            case 'paper':
+                status = 'win';
+                break;
+            case 'scissors':
+                status = 'draw';
+                break;
+        
+            default:
+                break;
+        }
+    
+        default:
+            break;
+    }
+    return status;
+}
+
+console.log(getWinStatus('paper', 'scissors'));
