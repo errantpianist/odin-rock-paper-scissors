007d28a2a2adf83e67c052872fdd85cc12e93112
Yu Zhou Zhang <yuzspanish@gmail.com>
2022-12-26 12:04:16 +0000

    implement random computer choice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    const choice = Math.floor(Math.random()* 3)
+    switch (choice) {
+        case 0:
+            return 'Rock';
+            break;
+        case 1:
+            return 'Paper';
+            break;
+    
+        case 2:
+            return 'Scissors';
+            break;
+    }
+}
+
+console.log(getComputerChoice());
